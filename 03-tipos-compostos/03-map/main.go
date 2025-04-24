e5f5d0b1badccbed8cb5aed73c62e07322ba2f5c
Lucas Schilin <schilin.lucas@gmail.com>
2025-04-24 05:49:39 -0300

    learn map type

--- 03-tipos-compostos/03-map/main.go
+++ 03-tipos-compostos/03-map/main.go
@@ -1,5 +1,35 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	var pessoas = map[string]int{}
+
+	pessoas["lucas"] = 24
+	pessoas["amanda"] = 24
+	pessoas["luiz"] = 57
+
+	fmt.Println(pessoas)
+
+	if idade, ok := pessoas["lucas"]; ok {
+		fmt.Println("Pessoa existe no map.", idade, ok)
+	} else {
+		fmt.Println("Pessoa NÃO existe no map.")
+	}
+
+	pessoas["lucas"]++
+
+	fmt.Println(pessoas["lucas"])
+
+	delete(pessoas, "lucas")
+
+	if idade, ok := pessoas["lucas"]; ok {
+		fmt.Println("Pessoa existe no map.", idade, ok)
+	} else {
+		fmt.Println("Pessoa NÃO existe no map.")
+	}
+
+	delete(pessoas, "diego")
+
+	fmt.Println(pessoas)
 }
