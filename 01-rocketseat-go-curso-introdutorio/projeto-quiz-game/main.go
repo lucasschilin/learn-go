5b8718650e9b701233fd71c896536cfa5f524586
Lucas Schilin <schilin.lucas@gmail.com>
2025-04-30 06:41:36 -0300

    implement user interaction, CSV reader

--- 01-rocketseat-go-curso-introdutorio/projeto-quiz-game/main.go
+++ 01-rocketseat-go-curso-introdutorio/projeto-quiz-game/main.go
@@ -1,7 +1,83 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"encoding/csv"
+	"fmt"
+	"os"
+)
+
+type Player struct {
+	Name  string
+	Email string
+}
+
+type Question struct {
+	Text    string
+	Options []string
+	Answer  int
+}
+
+type GameState struct {
+	Player    *Player
+	Questions []Question
+	Score     float32
+}
+
+func (g *GameState) Init() {
+	fmt.Println("Olá, seja bem vindo ao Quiz da Copa Do Mundo!")
+
+	player := Player{}
+
+	fmt.Println("Qual é o seu nome:")
+	name := ReadInteraction()
+	player.Name = name
+
+	fmt.Println("Qual é o seu endereço de e-mail:")
+	email := ReadInteraction()
+	player.Email = email
+
+	g.Player = &player
+
+	g.ProcessCSV()
+
+	fmt.Printf("Vamos ao jogo, %s", player.Name)
+}
+
+func (g *GameState) ProcessCSV() {
+	file, err := os.Open("questions.csv")
+	if err != nil {
+		panic("Erro ao abrir CSV")
+	}
+	defer file.Close()
+
+	reader := csv.NewReader(file)
+	records, err := reader.ReadAll()
+
+	if err != nil {
+		panic("Erro ao ler conteúdo do CSV")
+	}
+
+	for index, row := range records {
+		if index != 0 {
+			fmt.Println(row)
+			// TODO: Create Question{} and add into GameState{}
+		}
+	}
+}
+
+func ReadInteraction() string {
+	fmt.Print(">> ")
+	reader := bufio.NewReader(os.Stdin)
+	interaction, err := reader.ReadString('\n')
+	if err != nil {
+		panic("Erro ao ler a string")
+	}
+
+	return interaction
+}
 
 func main() {
-	fmt.Println("Olá")
+	game := &GameState{}
+	game.Init()
 }
