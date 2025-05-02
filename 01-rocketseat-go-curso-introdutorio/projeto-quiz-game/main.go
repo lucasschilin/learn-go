7d1260b14a21c963ac5f3681c2dda6b930370cf1
Lucas Schilin <schilin.lucas@gmail.com>
2025-05-02 06:50:10 -0300

    finshed the logic of Run function

--- 01-rocketseat-go-curso-introdutorio/projeto-quiz-game/main.go
+++ 01-rocketseat-go-curso-introdutorio/projeto-quiz-game/main.go
@@ -19,31 +19,13 @@ type Question struct {
 	Answer  int
 }
 
-type GameState struct {
-	Player    *Player
+type Game struct {
+	Player    Player
 	Questions []Question
 	Score     float32
 }
 
-func (g *GameState) Init() {
-	fmt.Println("Olá, seja bem vindo ao Quiz da Copa Do Mundo!")
-
-	player := Player{}
-
-	fmt.Println("Qual é o seu nome:")
-	name := ReadInteraction()
-	player.Name = name
-
-	fmt.Println("Qual é o seu endereço de e-mail:")
-	email := ReadInteraction()
-	player.Email = email
-
-	g.Player = &player
-
-	fmt.Printf("Vamos ao jogo, %s", player.Name)
-}
-
-func (g *GameState) ProcessCSV() {
+func (g *Game) ProcessCSV() {
 	file, err := os.Open("questions.csv")
 	if err != nil {
 		panic("Erro ao abrir CSV")
@@ -59,17 +41,66 @@ func (g *GameState) ProcessCSV() {
 
 	for index, row := range records {
 		if index != 0 {
-			fmt.Println(row)
-
+			correctAnswer, err := toInt(row[5])
+			if err != nil {
+				panic("Resposta inválida carregada do CSV")
+			}
 			question := Question{
 				Text:    row[0],
 				Options: row[1:5],
-				Answer:  toInt(row[5]),
+				Answer:  correctAnswer,
 			}
 
 			g.Questions = append(g.Questions, question)
 		}
 	}
+}
+
+func (g *Game) Init() {
+	fmt.Println("Olá, seja bem vindo ao Quiz da Copa Do Mundo!")
+
+	player := Player{}
+
+	fmt.Println("Qual é o seu nome:")
+	name := ReadInteraction()
+	player.Name = name
+
+	fmt.Println("Qual é o seu endereço de e-mail:")
+	email := ReadInteraction()
+	player.Email = email
+
+	g.Player = player
+
+	fmt.Printf("Vamos ao jogo, %s", player.Name)
+}
+
+func (g *Game) Run() {
+	for indexQ, question := range g.Questions {
+		fmt.Println(toStr(indexQ+1)+". -", question.Text)
+		for indexO, option := range question.Options {
+			fmt.Println("["+toStr(indexO+1)+"]", option)
+		}
+		fmt.Println("Escolha um alterantiva:")
+		var playerAnswer int
+		for {
+			interaction := ReadInteraction()
+			interaction = interaction[:len(interaction)-1]
+			answer, err := toInt(interaction)
+			if err != nil || answer > len(question.Options) || answer < 1 {
+				fmt.Printf("A resposta deve ser um inteiro entre %d e %d", 1, len(question.Options))
+			} else {
+				playerAnswer = answer
+				break
+			}
+		}
+		fmt.Printf("\n\n\n")
+
+		if playerAnswer == question.Answer {
+			g.Score += 10
+		}
+	}
+
+	fmt.Printf("Score = %f\n", g.Score/float32(len(g.Questions)))
 }
 
 func ReadInteraction() string {
@@ -83,20 +114,18 @@ func ReadInteraction() string {
 	return interaction
 }
 
-func toInt(s string) int {
-	i, err := strconv.Atoi(s)
-	if err != nil {
-		panic("Erro ao converter valor de string para inteiro")
-	}
+func toInt(s string) (int, error) {
+	return strconv.Atoi(s)
+}
 
-	return i
+func toStr(i int) string {
+	return strconv.Itoa(i)
 }
 
 func main() {
-	game := &GameState{}
+	game := &Game{}
 	go game.ProcessCSV()
 	game.Init()
-
-	fmt.Println(game.Questions)
+	game.Run()
 
 }
